a685e9be9339ca84ee0f06f664b44ee0c2db2453
MiguelNewtral <miguel.ros@newtral.es>
2026-03-03 15:58:01 +0100

    Updates

--- _app/immutable/entry/start.D52oqgDL.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BNiqFrR-.js";import{x as t}from"../chunks/BqKpkmdf.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.utKBPyVY.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DuKkLTT3.js";import{x as t}from"../chunks/BIxkyWwr.js";export{t as load_css,r as start};
